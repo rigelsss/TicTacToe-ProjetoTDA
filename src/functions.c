82be503c7333c4ddabac4298a4e5ae8c2bd2d080
rigel <rigelsouza@gmail.com>
2024-11-27 15:46:53 -0300

    feat: flow do jogo com correto funcionamento

--- src/functions.c
+++ src/functions.c
@@ -23,6 +23,179 @@ void logo_drawer()  {
 };
 
 
+int validarEntrada(char *entrada) {
+    int i = 0;
+
+    if (entrada[0] == '\0') {
+        return 0;
+    }
+
+    while (entrada[i] != '\0') {
+        if (!isdigit(entrada[i])) {
+            return 0; 
+        }
+        i++;
+    }
+    return 1; 
+}
+
+int validarPosicao(int *entrada) {
+    int i = 0;
+
+    if (entrada[0] == '\0') {
+        return 0;
+    }
+
+    while (entrada[i] != '\0') {
+        if (!isdigit(entrada[i])) {
+            return 0; 
+        }
+        i++;
+    }
+    return 1; 
+}
+
+
+
+void imprimeTabuleiro(char tabuleiro[3][3]) {
+    printf("\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf(" %c ", tabuleiro[i][j]); 
+            if (j < 2) {
+                printf("|"); 
+            }
+        }
+        printf("\n"); 
+        if (i < 2) {
+            printf("---|---|---\n"); 
+        }
+    }
+    printf("\n");
+}
+
+int validarJogada(char tabuleiro[3][3], int linha, int coluna)  {
+    if(linha >= 0 && linha < 3 && coluna >= 0 && coluna <3) {
+        if(tabuleiro[linha][coluna] == ' ') {
+                 return 1;
+
+            }     
+    }
+    return 0;
+
+}
+
+void realizarJogada(char tabuleiro[3][3], int linha, int coluna, char simbolo)    {
+    tabuleiro[linha][coluna] = simbolo;
+}
+
+
+int verificarVencedor(char tabuleiro[3][3]) {
+    for(int i = 0; i < 3; i++)  {
+        if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2] && tabuleiro[i][0] != ' ')
+            return 1;
+    
+        if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i] && tabuleiro[0][i] != ' ')
+            return 1;
+
+    }
+
+    if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2] && tabuleiro[0][0] != ' ')
+        return 1;
+    if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0] && tabuleiro[0][2] != ' ')
+        return 1;
+
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (tabuleiro[i][j] == ' ')
+                return 0; 
+        }
+    }
+
+    return -1;
+}
+
+char iniciarJogo()   {
+
+    int linha;
+    int coluna;
+    int resultado = 0;
+    int turno = 0;
+
+
+    typedef struct  {
+        char nome[50];
+        char simbolo;
+    } Player;
+
+
+
+    char tabuleiro[3][3] = {
+
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+
+    };
+
+
+    Player jogador[2];
+
+
+    printf("Digite o nome do Jogador 'X': ");
+    fgets(jogador[0].nome, sizeof(jogador[0].nome), stdin);
+    jogador[0].simbolo = 'X';
+    jogador[0].nome[strcspn(jogador[0].nome, "\n")] = '\0';
+
+
+    printf("Digite o nome do Jogador 'O': ");
+    fgets(jogador[1].nome, sizeof(jogador[turno].nome), stdin);
+    jogador[1].simbolo = 'O';
+    jogador[1].nome[strcspn(jogador[1].nome, "\n")] = '\0';
+
+
+    while(resultado == 0)   {
+        imprimeTabuleiro(tabuleiro);
+        
+        printf("%s (%c), escolha em qual linha jogar (1-3): ", jogador[turno].nome, jogador[turno].simbolo);
+        scanf("%d", &linha);
+
+        printf("%s (%c), escolha em qual coluna jogar (1-3): ", jogador[turno].nome, jogador[turno].simbolo);
+        scanf("%d", &coluna);
+
+
+
+        if(validarJogada(tabuleiro, linha - 1, coluna -1))  {
+            realizarJogada(tabuleiro, linha -1, coluna - 1, jogador[turno].simbolo);
+            resultado = verificarVencedor(tabuleiro);
+
+            if(resultado == 1)  {
+                imprimeTabuleiro(tabuleiro);
+                printf("Parabens %s, voce foi o vencedor!\n", jogador[turno].nome);
+                printf("\n\nPressione 'Enter' para voltar ao Menu principal.\n");
+                getchar();
+                break;
+            }
+
+            else if(resultado == -1)    {
+                imprimeTabuleiro(tabuleiro);
+                printf("O jogo terminou empatado.\n");
+                break;
+            }
+
+        turno = 1 - turno;       
+    }    
+
+        else    {
+            printf("\nJogada invalida %s, tente novamente:\n", jogador[turno].nome);
+        }
+    }
+    return 0;
+}
+
+
+
 void escreveRanking()  { 
 
     typedef struct  {
@@ -129,7 +302,7 @@ void escreveRanking()  {
 
     strftime(dataHora, sizeof(dataHora), "%d/%m/%Y %H:%M:%S", tempoLocal);
 
-    fprintf(fileRanking, "\nUltima aualizacao em: %s", dataHora);
+    fprintf(fileRanking, "\nUltima aualizacao em: %s UTC-3", dataHora);
 
     fclose(fileRanking);
 
@@ -143,22 +316,6 @@ void escreveRanking()  {
 }
 
 
-
-int validarEntrada(char *entrada) {
-    int i = 0;
-
-    if (entrada[0] == '\0') {
-        return 0;
-    }
-
-    while (entrada[i] != '\0') {
-        if (!isdigit(entrada[i])) {
-            return 0; 
-        }
-        i++;
-    }
-    return 1; 
-}
 
 
 void lerRanking() {
@@ -184,7 +341,7 @@ void lerRanking() {
 
     fclose(fileRanking); 
 
-    printf("\n\nPressione 'Enter' para voltar ao Menu principal \n");
+    printf("\n\nPressione 'Enter' para voltar ao Menu principal.");
     getchar();
 
     printf("\n");
@@ -198,8 +355,7 @@ void menuInicial() {
     
     int opcao = 0;
     char entrada[10];
-    
-    
+        
     do {
         
         printf("Tic Tac Toe Game - Menu\n");
@@ -225,7 +381,7 @@ void menuInicial() {
         {
             case 1:
                 printf("\nIniciando jogo...\n\n");
-                escreveRanking();
+                iniciarJogo();
                 break;
     
             case 2:
